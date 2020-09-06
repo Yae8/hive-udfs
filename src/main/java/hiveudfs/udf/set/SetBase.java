7cdacb03888161e902e366fae0c79acabee37632
sAkura844 <lemonlime844@gmail.com>
2020-09-06 16:27:18 +0900

    add set

---
+++ src/main/java/hiveudfs/udf/set/SetBase.java
@@ -0,0 +1,92 @@
+package hiveudfs.udf.set;
+
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ListObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.MapObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorUtils;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.StandardListObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.StandardMapObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector.Category;
+
+abstract class SetBase extends GenericUDF {
+
+    protected Category category;
+    protected ListObjectInspector list1OI;
+    protected ListObjectInspector list2OI;
+    protected MapObjectInspector map1OI;
+    protected MapObjectInspector map2OI;
+    protected PrimitiveObjectInspector primitive1OI;
+    protected PrimitiveObjectInspector primitive2OI;
+    protected StandardListObjectInspector stdListOI;
+    protected StandardMapObjectInspector stdMapOI;
+
+    abstract Object evaluateList(DeferredObject[] arguments) throws HiveException;
+    abstract Object evaluateMap(DeferredObject[] arguments) throws HiveException;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+        if (arguments.length != 2) {
+            throw new UDFArgumentException("The function " + getUdfName() + " takes 2 maps or lists.");
+        }
+
+        ObjectInspector firstOI = arguments[0];
+        ObjectInspector secondOI = arguments[0];
+
+        category = firstOI.getCategory();
+
+        if (secondOI.getCategory() != category) {
+            throw new UDFArgumentException("The function " + getUdfName() + " must either be all maps or all lists of the same type.");
+        }
+
+        switch (category) {
+            case LIST:
+                list1OI = (ListObjectInspector) firstOI;
+                list2OI = (ListObjectInspector) secondOI;
+                if (list1OI.getListElementObjectInspector().getCategory() != Category.PRIMITIVE || list2OI.getListElementObjectInspector().getCategory() != Category.PRIMITIVE) {
+                    throw new UDFArgumentException("The function " + getUdfName() + " takes only maps or lists of primitives.");
+                }
+                primitive1OI = (PrimitiveObjectInspector) list1OI.getListElementObjectInspector();
+                primitive2OI = (PrimitiveObjectInspector) list2OI.getListElementObjectInspector();
+                if (primitive1OI.getPrimitiveCategory().compareTo(primitive2OI.getPrimitiveCategory()) != 0) {
+                    throw new UDFArgumentException("The function " + getUdfName() + " must either be all maps or all lists of the same type");
+                }
+                stdListOI = ObjectInspectorFactory.getStandardListObjectInspector(
+                    ObjectInspectorUtils.getStandardObjectInspector(primitive1OI, ObjectInspectorUtils.ObjectInspectorCopyOption.JAVA));
+                return stdListOI;
+            case MAP:
+                map1OI = (MapObjectInspector) firstOI;
+                map2OI = (MapObjectInspector) secondOI;
+                if (map1OI.getMapKeyObjectInspector().getCategory() != Category.PRIMITIVE || map2OI.getMapKeyObjectInspector().getCategory() != Category.PRIMITIVE) {
+                    throw new UDFArgumentException("The function " + getUdfName() + " takes only maps or lists of primitives.");
+                }
+                primitive1OI = (PrimitiveObjectInspector) map1OI.getMapKeyObjectInspector();
+                primitive2OI = (PrimitiveObjectInspector) map2OI.getMapKeyObjectInspector();
+                if (primitive1OI.getPrimitiveCategory().compareTo(primitive2OI.getPrimitiveCategory()) != 0) {
+                    throw new UDFArgumentException("The function " + getUdfName() + " must either be all maps or all lists of the same type");
+                }
+                stdMapOI = ObjectInspectorFactory.getStandardMapObjectInspector(
+                        ObjectInspectorUtils.getStandardObjectInspector(primitive1OI, ObjectInspectorUtils.ObjectInspectorCopyOption.JAVA),
+                        ObjectInspectorUtils.getStandardObjectInspector(map1OI.getMapValueObjectInspector()));
+                return stdMapOI;
+            default:
+                throw new UDFArgumentException("The function " + getUdfName() + " takes only maps or lists.");
+        }
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[] arguments) throws HiveException {
+        switch (category) {
+            case LIST:
+                return evaluateList(arguments);
+            case MAP:
+                return evaluateMap(arguments);
+            default:
+                throw new HiveException("Only maps or lists are supported.");
+        }
+    }
+}
