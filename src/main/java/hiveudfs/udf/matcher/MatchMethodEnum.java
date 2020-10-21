c938389e96b61dc275dc2806f5d1dc64e24c6932
sAkura844 <lemonlime844@gmail.com>
2020-10-21 23:02:21 +0900

    add enum for match method

--- src/main/java/hiveudfs/udf/matcher/MatchMethodEnum.java
+++ src/main/java/hiveudfs/udf/matcher/MatchMethodEnum.java
@@ -1,4 +1,38 @@
 package hiveudfs.udf.matcher;
 
-public enum MatchMethod {
+public enum MatchMethodEnum {
+    PERFECT(0),
+    PARTIAL(1),
+    SUFFIX(2),
+    PREFIX(3),
+    SUBSET(10),
+    SUPERSET(11),
+    NO_PARTICULAR_ORDER(20),
+    ;
+
+    private final int num;
+
+    MatchMethodEnum(int num) {
+        this.num = num;
+    }
+
+    public int getNum() { return this.num; }
+
+    public static boolean isMember(int num) {
+        for (MatchMethodEnum matchMethod : values()) {
+            if (matchMethod.getNum() == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static MatchMethodEnum findByNum(int num) {
+        for (MatchMethodEnum matchMethod : values()) {
+            if (matchMethod.getNum() == num) {
+                return matchMethod;
+            }
+        }
+        return null;
+    }
 }
