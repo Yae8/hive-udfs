9fe3c85bf9908c8407a28b00d6c598ac66f9cc99
Yae8 <lemonlime844@gmail.com>
2020-10-27 01:42:23 +0900

    organize

---
+++ src/main/java/hiveudfs/udf/date/DatePatternType.java
@@ -0,0 +1,7 @@
+package hiveudfs.udf.date;
+
+final class DatePatternType {
+    public static final int DATE = 1;
+    public static final int TIMESTAMP = 2;
+    public static final int TIMESTAMP_TZ = 3;
+}
---
+++ src/main/java/hiveudfs/udf/date/DateUnitType.java
@@ -0,0 +1,12 @@
+package hiveudfs.udf.date;
+
+final class DateUnitType {
+    public static final int YEAR = 1;
+    public static final int MONTH = 2;
+    public static final int DATE = 3;
+    public static final int HOUR = 4;
+    public static final int MINUTE = 5;
+    public static final int SECOND = 6;
+    public static final int QUARTER = 7;
+    private DateUnitType (){}
+}
